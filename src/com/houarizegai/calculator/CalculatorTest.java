2f635875164730db1898d6d671893e7fc21b286c
PopescuGeorge1 <>
2021-01-13 09:40:32 +0200

    Update CalculatorTest.java

--- src/com/houarizegai/calculator/CalculatorTest.java
+++ src/com/houarizegai/calculator/CalculatorTest.java
@@ -8,33 +8,20 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-	private Calculator calculator;
-
-	/*
-	 * create object before compilation
-	 */	@Before
-	public void createObj(){
-		calculator = new Calculator();
-	}
-	
-	/*
-	 * testCalc() test method
-	*/ 
-	@Test
+	@org.junit.jupiter.api.Test
 	public void testCalc() {
 		double first =3;
 		String second="5";
+		Calculator calculator = new Calculator();
 
-		try {
-		Assertions.assertEquals(5,calculator.calc(first,second,'+'));
+		Assertions.assertEquals(8,calculator.calc(first,second,'+'));
 		Assertions.assertEquals(-2,calculator.calc(first,second,'-'));
 		Assertions.assertEquals(15,calculator.calc(first,second,'*'));
 		Assertions.assertEquals(0.6,calculator.calc(first,second,'/'));
-		Assertions.assertEquals(((3/5)*100),calculator.calc(first,second,'%'));
+		Assertions.assertEquals((3%5),calculator.calc(first,second,'%'));
 		Assertions.assertEquals(243,calculator.calc(first,second,'^'));
-		}catch(Exception e) {}
+
 		
 	}
-	//all tests passed
 
 }
