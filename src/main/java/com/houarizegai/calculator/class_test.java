8097a47656bb91146728074afceb5d7b809d5c65
Abdus Shafi <abdusshsfi99@gmail.com>
2021-11-29 22:51:06 +0600

    new added class

---
+++ src/main/java/com/houarizegai/calculator/class_test.java
@@ -0,0 +1,26 @@
+package com.houarizegai.calculator;
+
+public class class_test {
+    int a,b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public class_test(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
