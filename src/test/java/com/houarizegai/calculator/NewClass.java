043a8f07a5d9dfeb40ff55ca2e1b40bf3493059a
Shireesh <ithineni@gmail.com>
2022-02-03 17:49:39 +1100

    develop branch change

--- src/test/java/com/houarizegai/calculator/NewClass.java
+++ src/test/java/com/houarizegai/calculator/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("to check new push and pull requests");
+		System.out.println("Testing to check changes from develop branch");
 
 	}
 
