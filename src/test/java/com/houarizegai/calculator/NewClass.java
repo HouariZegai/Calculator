af1de82ac9437d50421ba4e3218cf71e52008d40
Shireesh <ithineni@gmail.com>
2022-02-03 17:41:50 +1100

    final commit

---
+++ src/test/java/com/houarizegai/calculator/NewClass.java
@@ -0,0 +1,11 @@
+package com.houarizegai.calculator;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("to check new push and pull requests");
+
+	}
+
+}
