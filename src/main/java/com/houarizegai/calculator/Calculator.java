40f656bbcbfb785ad30a328530b91d7561e763c6
ithineni <85816660+ithineni@users.noreply.github.com>
2022-02-03 17:14:24 +1100

    Update Calculator.java

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -13,6 +13,7 @@ import java.lang.Math;
 public class Calculator {
 //This is for test calculator
 	//To check changes on git
+	//To check pull the code from Git
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
