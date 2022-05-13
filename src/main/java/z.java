497da43b6da6492edfafaafee1f384a1ab3b6c09
Arakis91 <blackspirits.ghosts@gmail.com>
2022-05-13 14:44:27 +0600

    commit

---
+++ src/main/java/z.java
@@ -0,0 +1,14 @@
+public class z {
+    int a;
+    public z(int a) {
+        this.a=a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
