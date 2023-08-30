a028dfeefb194ecbc942135d28a852733ee0b7cc
hemanth052 <115633007+hemanth052@users.noreply.github.com>
2023-08-30 17:24:09 +0530

    Update App.java

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -2,7 +2,7 @@ package com.houarizegai.calculator;
 
 import com.houarizegai.calculator.ui.CalculatorUI;
 
-public class App {
+public class MyApp {
 
     public static void main(String[] args) {
         new CalculatorUI();
