3ac4bf7a9b727b28c72fd21e53574d56d016d4d8
Shobhith Lakkakula <shobhith.lakkakula@tcpwave.com>
2023-08-31 23:37:37 +0530

    first commit for contribution

--- src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
+++ src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
@@ -460,7 +460,7 @@ public class CalculatorUI {
                 } else {
                     inputScreen.setText(String.valueOf(typedValue));
                 }
-                selectedOperator = '√';
+                selectedOperator = '\u221A';
                 addToDisplay = false;
             }
         });
