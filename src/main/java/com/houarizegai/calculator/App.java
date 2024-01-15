e1fe3ab21c169e2e70996e7347192230e3a733f7
savvy1525 <savvypostal@outlook.com>
2024-01-15 02:05:25 +0100

    ...

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -3,7 +3,6 @@ package com.houarizegai.calculator;
 import com.houarizegai.calculator.ui.CalculatorUI;
 
 public class App {
-    //HelloWorld..
     public static void main(String[] args) {
         new CalculatorUI();
     }
